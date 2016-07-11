c7fee6e052090a5748324a2dda6193186011d251
Thomas Mooney <tmooney@genome.wustl.edu>
2016-07-11 16:10:42 -0500

    A somewhat pared down version of what `lein new` does.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Toy Counting Game
+
+A Clojure library designed to help me learn Clojure...
+and that's about it!
+
+But you can a play a game if you like.
+
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject toy_counting_game "0.1.0-SNAPSHOT"
+  :description "Some toy counting game so I can learn Clojure"
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main toy-counting-game.core)
